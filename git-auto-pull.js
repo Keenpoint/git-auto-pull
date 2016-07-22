38490d34deea149cdaa12dcf70b5d1f3f8b12ad5
Gilles Henaux <gill.henaux@gmail.com>
2016-07-22 14:55:57 +0200

    Adding support for other commands

--- git-auto-pull.js
+++ git-auto-pull.js
@@ -1,24 +1,41 @@
-var http = require("http");
-var exec = require("child_process").exec;
-var path = require('path');
+const http = require("http");
+const exec = require("child_process").exec;
+const path = require("path");
+const async = require("async");
 
 const projectPath = process.argv[2];
 const absolutePath = path.join(__dirname, projectPath);
 
-var execute = function(callback) {
-    exec(`cd ${absolutePath} && git pull`, function(err, stdout, stderr) {
-        callback(err, ` stdout: ${stdout} \n stderr: ${stderr}`);
-    });
-}
+const cmds = ["git pull"].concat(process.argv.filter((arg, index) => { return index > 2; }));
+
+const execCmds = cmds.map((cmd) => {
+	return function(callback) {
+		exec(`cd ${absolutePath} && ${cmd}`, (err, stdout, stderr) => {
+			if(err) return callback(err);
+			callback(null, `--- ${cmd} ---:\n stdout: ${stdout} \n stderr: ${stderr}\n`);
+		});
+	};
+});
+
+const updateProject = function(callback) {
+	async.series(
+		execCmds
+	, function(err, results) {
+		if(err) return callback(err);
+		return callback(null, results.join(""));
+	});
+};
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    console.log("An event has been detected on the listened port: starting execution...")
 
-    execute(function(err, result) {
-        if (err) console.error(`exec error: ${err}`);
-        console.log(result);
+	updateProject((err, result) => {
+	    if(err) console.error(`exec error: ${err}`);
+        if(result) console.log(result);
         res.end();
-    });
+	});
 
 }).listen(1337);
-console.log('Git-auto-pull is running');
+console.log("Git-auto-pull is running");
+
